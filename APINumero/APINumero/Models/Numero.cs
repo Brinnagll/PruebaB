395bf3211ce5a70a5ab7ee17084feca691121b05
Brinna Francesca Gorena Llado <a2019119323@estudiantes.upsa.edu.bo>
2020-12-12 02:27:22 -0400

    clase numero creada

---
+++ APINumero/APINumero/Models/Numero.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace APINumero.Models
+{
+    public class Numero
+    {
+            [Key]
+            public int numero { get; set; }
+            public string mensaje { get; set; }
+        
+    }
+}
